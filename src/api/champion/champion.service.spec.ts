cf8b509dfc4430b27daab4e461c730804f56cefa
Leonardo Ruiz <leonardo.ruizsua@gmail.com>
2021-05-02 02:39:32 -0500

    test(champion): added unit tests to champion service.

--- src/api/champion/champion.service.spec.ts
+++ src/api/champion/champion.service.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { of } from 'rxjs';
+import { mocks } from '../../test/api-mocks';
 import { Region } from '../../common/enums';
-import { KeyValue } from '../../common/types';
+import { ChampionInfo, KeyValue } from '../../common/types';
 import { ApiClient } from '../client/api-client';
 import { ChampionService } from './champion.service';
+import { CHAMPION_ROTATIONS } from './routes';
 
 class ApiClientMock {
   executeGet(
@@ -17,6 +19,7 @@ class ApiClientMock {
 }
 describe('ChampionService', () => {
   let service: ChampionService;
+  let apiClient: ApiClient;
 
   beforeEach(async () => {
     const apiClientProvider = {
@@ -28,9 +31,26 @@ describe('ChampionService', () => {
     }).compile();
 
     service = module.get<ChampionService>(ChampionService);
+    apiClient = module.get<ApiClient>(ApiClient);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should get champion rotations successfully', async () => {
+    const apiClientSpy = jest
+      .spyOn(apiClient, 'executeGet')
+      .mockImplementationOnce(
+        () =>
+          new Promise((resolve) =>
+            setTimeout(() => resolve(mocks[CHAMPION_ROTATIONS]), 2000),
+          ),
+      );
+
+    const result = await service.getChampionRotations(Region.LAN);
+
+    expect(apiClientSpy).toBeCalledWith(Region.LAN , CHAMPION_ROTATIONS);
+    expect(result).toBe(mocks[CHAMPION_ROTATIONS]);
   });
 });
---
+++ src/test/api-mocks.ts
@@ -0,0 +1,34 @@
+export const mocks = {
+    '/lol/platform/v3/champion-rotations': {
+        'freeChampionIds': [
+            14,
+            34,
+            37,
+            38,
+            74,
+            77,
+            85,
+            96,
+            120,
+            145,
+            150,
+            246,
+            497,
+            876,
+            887
+        ],
+        'freeChampionIdsForNewPlayers': [
+            18,
+            81,
+            92,
+            141,
+            37,
+            238,
+            19,
+            45,
+            25,
+            64
+        ],
+        'maxNewPlayerLevel': 10
+    }
+}
